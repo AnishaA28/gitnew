a46ee356732e03114fa32e32c2e99ac1b97d6047
AnishaA28 <ani13011997@gmail.com>
2025-01-24 14:36:53 +0530

    modified reflexpro1

--- reflexpro1/reflexpro1.py
+++ reflexpro1/reflexpro1.py
@@ -118,7 +118,35 @@ def index() -> rx.Component:
             width="100%",
             
         ),
-       
+        rx.scroll_area(
+        rx.flex(
+        rx.text(
+            """Three fundamental aspects of typography are legibility, readability, and
+        aesthetics. Although in a non-technical sense “legible” and “readable”
+        are often used synonymously, typographically they are separate but
+        related concepts.""",
+        ),
+        rx.text(
+            """Legibility describes how easily individual characters can be
+        distinguished from one another. It is described by Walter Tracy as “the
+        quality of being decipherable and recognisable”. For instance, if a “b”
+        and an “h”, or a “3” and an “8”, are difficult to distinguish at small
+        sizes, this is a problem of legibility.""",
+        ),
+        rx.text(
+            """Typographers are concerned with legibility insofar as it is their job to
+        select the correct font to use. Brush Script is an example of a font
+        containing many characters that might be difficult to distinguish. The
+        selection of cases influences the legibility of typography because using
+        only uppercase letters (all-caps) reduces legibility.""",
+        ),
+        direction="column",
+        spacing="4",
+    ),
+    type="always",
+    scrollbars="vertical",
+    style={"height": 180},
+        ),
         
         rx.box(rx.button(
             rx.icon(tag="heart"),  
